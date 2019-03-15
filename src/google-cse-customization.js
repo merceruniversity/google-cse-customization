f87ea84bf50dad180225aa0c2ce554f204831c0a
Todd Sayre <captain.friendly@gmail.com>
2019-03-15 11:30:04 -0400

    Looking pretty good

--- src/google-cse-customization.js
+++ src/google-cse-customization.js
@@ -1,28 +1,43 @@
 import elementReady from 'element-ready';
-import debounce from 'debounce-promise';
 
 import './google-cse-customization.scss';
 
-const setAttribute = (target, attr, val) => target.setAttribute(attr, val);
-const debouncedSetAttribute = debounce(setAttribute, 100);
+const removeAll = coll => coll.forEach(item => item.remove());
 
-const gscInputCallback = (mutationsList, observer) => {
-  for (const mutation of mutationsList) {
-    console.log(mutation);
-    if ('style' === mutation.attributeName && '' !== mutation.target.getAttribute('style')) {
-      debouncedSetAttribute(mutation.target, 'style', '');
+const isGcseStyling = node => ('LINK' === node.tagName &&
+    'undefined' !== node.href &&
+    (/^https:\/\/www\.google\.com\/cse\/static\/element\//i.test(node.href) ||
+      /^https:\/\/www\.google\.com\/cse\/static\/style\/look\//i.test(node.href))) ||
+  ('STYLE' === node.tagName &&
+    (/\.gsc-control-cse/i.test(node.innerHTML) ||
+      /\.gscb_a/i.test(node.innerHTML)));
+
+const removeGcseStyleNodesFromCollection = coll => {
+  if (!coll) return;
+  const nodesToRemove = [];
+  for (const node of coll) {
+    // console.log(node);
+    if (isGcseStyling(node)) {
+      nodesToRemove.push(node);
     }
   }
+  // console.log(nodesToRemove);
+  removeAll(nodesToRemove);
 }
 
-(async () => {
-  const gscInput = await elementReady('input.gsc-input');
-  console.log(gscInput);
-  gscInput.setAttribute('placeholder', 'Search');
-  gscInput.setAttribute('style', 'value');
+const gcseCallBack = () => {
+  (async () => {
+    const gscInput = await elementReady('input.gsc-input');
+    // console.log(gscInput);
+    gscInput.setAttribute('placeholder', 'Search');
+    gscInput.setAttribute('style', 'value');
+  })();
 
-  const gscInputObserver = new MutationObserver(gscInputCallback);
-  gscInputObserver.observe(gscInput, {
-    attributes: true
-  });
-})();
+  const headChildren = document.head.children;
+  // console.log([...Array.from(headChildren)]);
+  removeGcseStyleNodesFromCollection(headChildren);
+}
+
+window.__gcse = {
+  callback: gcseCallBack
+};
