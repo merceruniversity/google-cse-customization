444e32039e56c22ec29f94fa9eae2295d02b91ed
Todd Sayre <captain.friendly@gmail.com>
2019-03-15 11:45:58 -0400

    Keep the search from from being re-styled

--- src/google-cse-customization.js
+++ src/google-cse-customization.js
@@ -6,8 +6,8 @@ const removeAll = coll => coll.forEach(item => item.remove());
 
 const isGcseStyling = node => ('LINK' === node.tagName &&
     'undefined' !== node.href &&
-    (/^https:\/\/www\.google\.com\/cse\/static\/element\//i.test(node.href) ||
-      /^https:\/\/www\.google\.com\/cse\/static\/style\/look\//i.test(node.href))) ||
+    (/\/cse\/static\/element\//i.test(node.href) ||
+      /\/cse\/static\/style\/look\//i.test(node.href))) ||
   ('STYLE' === node.tagName &&
     (/\.gsc-control-cse/i.test(node.innerHTML) ||
       /\.gscb_a/i.test(node.innerHTML)));
@@ -25,19 +25,53 @@ const removeGcseStyleNodesFromCollection = coll => {
   removeAll(nodesToRemove);
 }
 
+const gscInputMutationCallback = mutationsList => {
+  for (const mutation of mutationsList) {
+    // console.log(mutation);
+    if ('style' === mutation.attributeName && '' !== mutation.target.getAttribute('style')) {
+      mutation.target.style = '';
+    }
+  }
+}
+const gscInputObserver = new MutationObserver(gscInputMutationCallback);
+
 const gcseCallBack = () => {
+  const headChildren = document.head.children;
+  // console.log([...Array.from(headChildren)]);
+  removeGcseStyleNodesFromCollection(headChildren);
+
   (async () => {
     const gscInput = await elementReady('input.gsc-input');
     // console.log(gscInput);
     gscInput.setAttribute('placeholder', 'Search');
-    gscInput.setAttribute('style', 'value');
+    gscInputObserver.observe(gscInput, {
+      attributes: true
+    });
   })();
-
-  const headChildren = document.head.children;
-  // console.log([...Array.from(headChildren)]);
-  removeGcseStyleNodesFromCollection(headChildren);
 }
 
 window.__gcse = {
   callback: gcseCallBack
 };
+
+// import elementReady from 'element-ready';
+// import debounce from 'debounce-promise';
+
+// import './google-cse-customization.scss';
+
+// const setAttribute = (target, attr, val) => target.setAttribute(attr, val);
+// const debouncedSetAttribute = debounce(setAttribute, 100);
+
+
+
+// (async () => {
+//   const gscInput = await elementReady('input.gsc-input');
+//   console.log(gscInput);
+//   gscInput.setAttribute('placeholder', 'Search');
+//   gscInput.setAttribute('style', 'value');
+
+//   const gscInputObserver = new MutationObserver(gscInputCallback);
+//   gscInputObserver.observe(gscInput, {
+//     attributes: true
+//   });
+// })();
